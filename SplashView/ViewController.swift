e0a573c3727694281fca040eae562d6d4b34d7b7
hongchenxi <hongchenxi86@gmail.com>
2016-11-23 16:04:31 +0800

    Initial Commit

---
+++ SplashView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplashView
+//
+//  Created by 晨希 on 11/23/16.
+//  Copyright © 2016 cx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
